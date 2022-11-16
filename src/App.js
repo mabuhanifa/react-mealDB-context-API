6884b3176e01d6e4b48d45bddc60d35a4afd88bc
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-16 23:45:27 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useMeal } from "./api/context";
 import "./App.css";
 
 function App() {
-  const { state:{meals}, dispatch } = useMeal();
-  const loadData = () => {
+  const {
+    state: { meals },
+    dispatch,
+  } = useMeal();
+
+  const loadData = (data) => {
     dispatch({
       type: "ADD_MEALS",
-      payload: { new: "data" },
+      payload: data,
     });
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get(
+        "http://themealdb.com/api/json/v1/1/lookup.php?i=52772"
+      );
+      dispatch({
+        type: "ADD_MEALS",
+        payload: data,
+      });
+    };
+    fetchData();
+  }, [dispatch]);
 
   return (
     <div>
